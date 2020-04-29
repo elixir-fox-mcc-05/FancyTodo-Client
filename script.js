3287069c8f50ee275143d86c1929bee61caf3bc3
yodji09 <yodji09@gmail.com>
2020-04-30 01:52:44 +0700

    complete feature and a little styling

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const baseUrl = 'http://localhost:3000'
 
 $( document ).ready(function(){
-    authorize()
+    landingPage()
     $("#login-form").submit(function(event) {
         event.preventDefault()
         $.ajax({
@@ -14,7 +14,6 @@ $( document ).ready(function(){
         })
             .done(data => {
                 localStorage.setItem('token', data.Token)
-                authorize()
             })
             .fail(err => {
                 console.log(err.responseJSON.msg);
@@ -31,13 +30,34 @@ $( document ).ready(function(){
             },
         })
             .done(data => {
-                localStorage.setItem('Message', "succes create account")
+                localStorage.setItem("Message", "Succes Create Account")
             })
             .fail(err => {
                 console.log(err.responseJSON.msg);
             })
     })
 })
+function landingPage(){
+    if(localStorage.getItem('token')){
+        $('#login-page').hide()
+        $('#main-page').show()
+        $('#register-page').hide()
+        $('#navbar-page').hide()
+        $('#navbar-user').show()
+        $('.create-todo').hide()
+        $('.edit-todo').hide()
+        fetchTodo()
+    } else {
+        $('#login-page').hide()
+        $('#navbar-user').hide()
+        $('#navbar-page').show()
+        $('#main-page').hide()
+        $('#register-page').show()
+        $('.edit-todo').hide()
+        $('.create-todo').hide()
+    }
+}
+
 function authorize(){
     if(localStorage.getItem('token')){
         $('#login-page').hide()
@@ -45,28 +65,110 @@ function authorize(){
         $('#register-page').hide()
         $('#navbar-page').hide()
         $('#navbar-user').show()
-        fetchTodo()
+        $('.create-todo').hide()
+        $('.edit-todo').hide()
     } else {
         $('#login-page').hide()
         $('#navbar-user').hide()
         $('#navbar-page').show()
         $('#main-page').hide()
-        $('#register-page').hide()
+        $('#register-page').show()
+        $('.edit-todo').hide()
+        $('.create-todo').hide()
     }
 }
 function logout(){
-    localStorage.clear()
-    authorize()
+    const auth2 = gapi.auth2.getAuthInstance()
+    auth2.signOut().then(function () {
+        localStorage.clear()
+        $('#login-page').hide()
+        $('#navbar-user').hide()
+        $('#navbar-page').show()
+        $('#main-page').hide()
+        $('#register-page').show()
+        $('.edit-todo').hide()
+        $('.create-todo').hide()
+    })
 }
 function login(){
     $('#login-page').show()
     $('#main-page').hide();
     $('#register-page').hide()
+    $('.create-todo').hide()
+    $('.edit-todo').hide()
 }
 function register(){
     $('#register-page').show()
-    $('#login-page').hide();
-    $('#main-page').hide();
+    $('#login-page').hide()
+    $('#main-page').hide()
+    $('.create-todo').hide()
+    $('.edit-todo').hide()
+}
+function homeUser(){
+    $('#main-page').show();
+    $('.create-todo').hide()
+    $('.edit-todo').hide()
+}
+function create(){
+    $('.create-todo').show()
+    $('#main-page').hide()
+    $('.edit-todo').hide()
+}
+function GetTodo(value){
+    $('#main-page').hide()
+    $('.create-todo').hide()
+    $('.edit-todo').show()
+    $('#form-input-edit').empty();
+    const token = localStorage.token
+    $.ajax({
+        method : "get",
+        url : baseUrl + `/todos/${value}`,
+        headers: {
+            token,
+        },
+    })
+        .done(response=> {
+            let Todos = response.Todos
+            $('#form-input-edit').append(`
+            <input type="text" id="title-edit" value="${Todos.title}" />
+            <input type="text" id="description-edit" value="${Todos.description}" />
+            <select type="radio" id="status-edit" >
+                <option value="true" ${Todos.status ? "selected" : ""}>Complete</option>
+                <option value="false" ${!Todos.status ? "selected" : ""}>Uncomplete</option>
+            </select>
+            <input type="date" id="due_date-edit" value="${Todos.due_date}">
+            <input type="hidden" id="id-edit"value="${Todos.id}">
+            <button type="submit">Submit Todo</button>
+            `)
+        })
+}
+
+function edit(){
+    const token = localStorage.getItem('token')
+    const title = $('#title-edit').val()
+    const description = $('#description-edit').val()
+    const status = $('#status-edit').val()
+    const due_date = $('#due_date-edit').val()
+    const id = $("#id-edit").val()
+    $.ajax({
+        method : "put",
+        url : baseUrl + `/todos/${id}`,
+        headers : {
+            token,
+        },
+        data : {
+            title,
+            description,
+            status,
+            due_date
+        }
+    })
+        .done(response => {
+            $('#main-page').show();
+        })
+        .fail(err=>{
+            console.log(err.responseJSON.msg)
+        })
 }
 
 function fetchTodo(){
@@ -77,33 +179,20 @@ function fetchTodo(){
             token : localStorage.token
         }
     })
-    .done(data => {
-        data.Todos.forEach(element => {
+        .done(data => {
+            data.Todos.forEach(element => {
+                $("#table-todo").append(`
+                    <tr>
+                        <td>${element.title}</td>
+                        <td>${element.description}</td>
+                        <td>${element.status ? "Complete" : "Uncomplete"}</td>
+                        <td>${element.due_date}</td>
+                        <td><button type="button" class="btn btn-outline-primary" onclick="GetTodo(${element.id})">EDIT</button> || <button type="button" class="btn btn-outline-danger" onclick="deleteTodo(${element.id})">DELETE</button></td>
+                    </tr>
+                `)
+            })
             $(".main-container").append(`
-                <div class="todo">
-                    <h1 style="text-align: center;"></h1>
-                    <table border>
-                        <thead>
-                            <th>Todo Title</th>
-                            <th>Todo Description</th>
-                            <th>Todo Status</th>
-                            <th>Todo Date</th>
-                            <th>Action</th>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>${element.title}</td>
-                                <td>${element.description}</td>
-                                <td>${element.status ? "Complete" : "Uncomplete"}</td>
-                                <td>${element.due_date}</td>
-                                <td>${element.id}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            `)
-        })
-        $(".main-container").append(`
+                <br>
                 <div id="weather">
                     <p>Location = Depok</p>
                     <p>Date = ${data.Weather.dataseries[0].date}</p>
@@ -111,8 +200,75 @@ function fetchTodo(){
                     <p>Max Wind = ${data.Weather.dataseries[0].wind10m_max} m/s</p>
                 </div>
             `)
+        })
+        .fail(err => {
+            console.log(err.responseJSON.msg)
+        })
+}
+
+function deleteTodo(value){
+    const token = localStorage.token
+    $.ajax({
+        method : "delete",
+        url : baseUrl + `/todos/${value}`,
+        headers : {
+            token : localStorage.token
+        }
     })
-    .fail(err => {
+        .done(response => {
+            console.log("Succes Delete Data")
+        })
+        .fail(err => [
+            console.log(err.responseJSON.msg)
+        ])
+}
+
+function createTodo(event) {
+    event.preventDefault()
+    const token = localStorage.getItem('token')
+    const title = $('#title').val()
+    const description = $('#description').val()
+    const due_date = $('#due_date').val()
+
+    $.ajax({
+      method: 'post',
+      url: baseUrl + '/todos',
+      headers: {
+        token
+      },
+      data: {
+        title,
+        description,
+        due_date
+      }
+    })
+      .done(function (response) {
+        const Todos = response.Todos
+        $('#table-todo').append(`
+          <td>${Todos.title}</td>
+          <td>${element.description}</td>
+          <td>${element.due_date}</td>
+        `)
+      })
+      .fail(function (err) {
         console.log(err.responseJSON.msg)
+      })
+  }
+
+function onSignIn(googleUser) {
+    var id_token = googleUser.getAuthResponse().id_token
+    $.ajax({
+        method: 'post',
+        url : baseUrl + '/user/google-login',
+        headers : {
+            google_token : id_token
+        },
     })
+        .done(data => {
+            localStorage.setItem('token', data.Token)
+            authorize()
+        })
+        .fail(err => {
+            console.log(err)
+        })
 }
