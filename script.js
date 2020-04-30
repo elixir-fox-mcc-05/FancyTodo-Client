b8c88f45c19e5c04ed373afdb741021d4a53f87e
yaw96_ <yaw.networks@gmail.com>
2020-04-30 22:42:07 +0700

    update (edit) feature complete

--- script.js
+++ script.js
@@ -85,12 +85,48 @@ $(document).ready(function () {
             });
             
         })
-    })    
+    })
+
+    $(".edit-todo-form").submit(function(event) {
+
+        let id = $("#edit-todo-id").attr('value')
+        let title = $("#edit-todo-title").attr('value')
+        let description = $("#edit-todo-description").attr('value')
+        let due_date = $("#edit-todo-due-date").attr('value')
+
+        event.preventDefault();
+
+        $.ajax({
+            method : "put",
+            url : baseUrl + "/todos/" + id,
+            headers : {
+                token : localStorage.token
+            },
+            data : {
+                title : $("#edit-todo-title").val(),
+                description : $("#edit-todo-description").val(),
+                due_date : $("#edit-todo-due-date").val(),
+            }
+        })
+        .done(data => {
+            $("#edit-todo-title").val("")
+            $("#edit-todo-description").val("")
+            $("#edit-todo-due-date").val("")
+            auth()
+            
+        })
+        .fail(err => {
+            err.responseJSON.errors.forEach(element => {
+                $("#edit-todo-status").text(element.message)
+            });
+            
+        })
+    })
 });
 
 /**
  * ===========================================
- * Auth, Button : login, register, cancel, create, update, logout
+ * Auth, BUTTON : login, register, cancel, create, logout
  * ===========================================
  */
 
@@ -102,7 +138,6 @@ const auth = () => {
         $("#login-page").hide()
         $("#main-page").show()
         $("#create-todo-form").hide()
-        $("#edit-todo-form").hide()
         fetchToDos()
     } else {
         $("#landing-page").show()
@@ -110,7 +145,6 @@ const auth = () => {
         $("#login-page").hide()
         $("#main-page").hide()
         $("#create-todo-form").hide()
-        $("#edit-todo-form").hide()
     }
 }
 
@@ -149,7 +183,7 @@ const logout = () => {
 
 /**
  * ===========================================
- * Fetch, Create, Delete
+ * Fetch, Update, Delete
  * ===========================================
  */
 
@@ -168,25 +202,48 @@ const fetchToDos = () => {
         data.ToDos.forEach(element => {
             let date = new Date(element.due_date)
             $(".card").append(`
-            <div class="card-body">
+            <div id="card-${element.id}" class="card-body">
 
                 <h4 class="card-title">${element.title}</h4>
                 <div class="alert alert-warning" role="alert">
-                <strong>Due date : </strong>${date.getDate()}-${date.getMonth()}-${date.getFullYear()}
+                <strong>Due date : </strong>${element.due_date}
                 </div>
                 <h6 class="card-subtitle mb-2 text-muted">${element.status ? "COMPLETED !" : "not yet completed"}</h6>
                 <p class="card-text">
                 ${element.description}
                 </p>
-                <button type="button" class="btn btn-primary" onclick="updateToDo(${element.id})">Edit</button>
+                <button id="edit-todo-${element.id}" type="button" class="btn btn-primary" onclick="updateToDo('${element.id}', '${element.title}', '${element.description}', '${element.due_date}')">Edit</button>
                 <button type="button" class="btn btn-danger" onclick="deleteToDo(${element.id})">Delete</button><br><br>
+                
             </div>
         `)
         });
+        
     })
     .fail(err => {
         console.log(err.responseJSON.message);
     })
+}
+
+const updateToDo = (id, title, description, due_date) => {
+
+    $(`#edit-todo-${id}`).hide()
+    $(`#card-${id}`).append(`
+    <form class="edit-todo-form">
+        <input id="edit-todo-id" value="${id}" hidden>
+        <h2>Edit "${title}"</h2>
+        <label>Title :</label><br>
+        <input id="edit-todo-title" class="form-control" type="text" placeholder="todo title" value="${title}" required autofocus><br>
+        <label>Description :</label><br>
+        <input id="edit-todo-description" class="form-control" type="text" placeholder="description here" value="${description}" required><br>
+        <label>Due date :</label><br>
+        <input id="edit-todo-due-date" class="form-control" type="date" placeholder="date" value="${due_date}" required><br>
+        <input class="btn btn-primary" type="submit" value="Ok, done editing">
+        <input id="edit-todo-cancel" class="btn btn-outline-warning" type="button" value="Cancel" onclick="cancel()"><br><br>
+        <p id="edit-todo-status" class="alert alert-info" role="alert"></p>
+    </form>
+    `)
+
 }
 
 const deleteToDo = (id) => {
