40cc7a0c7af21f8d2fe729fb20dde79ea41a0b64
yaw96_ <yaw.networks@gmail.com>
2020-04-30 17:46:30 +0700

    create, fetch, delete, update(on progress)

--- script.js
+++ script.js
@@ -3,34 +3,8 @@ let baseUrl = "http://localhost:3000"
 $(document).ready(function () {
     auth()
 
-    $("#register-button").click(function() {
-        $("#landing-page").hide()
-        $("#register-page").show()
-        $("#login-page").hide()
-    })
-
-    $("#register-cancel").click(function() {
-        $("#landing-page").show()
-        auth()
-    })
-
-    $("#login-button").click(function() {
-        $("#landing-page").hide()
-        $("#register-page").hide()
-        $("#login-page").show()
-    })
-
-    $("#login-cancel").click(function() {
-        $("#landing-page").show()
-        auth()
-    })
-
-    $("#logout-button").click(function() {
-        localStorage.clear()
-        auth()
-    })
-
     $("#register-form").submit(function(event){
+
         event.preventDefault();
 
         $.ajax({
@@ -42,10 +16,12 @@ $(document).ready(function () {
             }
         })
         .done(data => {
-            $("#register-status").text("Success register..");
+            $("#register-status").text("Success Register...");
+            $("#register-to-login").show()
         })
         .fail(err => {
             $("#register-status").text(err.responseJSON.errors);
+            $("#register-to-login").hide()
         })
         .always(_ => {
             $("#register-email").val("")
@@ -54,6 +30,7 @@ $(document).ready(function () {
     })
 
     $("#login-form").submit(function(event){
+
         event.preventDefault();
 
         $.ajax({
@@ -65,7 +42,7 @@ $(document).ready(function () {
             }
         })
         .done(data => {
-            $("#login-status").text("Success login..");
+            $("#login-status").text("");
             // console.log(data.token);
             localStorage.setItem("token", data.token);
             auth()
@@ -78,7 +55,44 @@ $(document).ready(function () {
             $("#login-password").val("")
         })
     })
+
+    $("#create-todo-form").submit(function(event) {
+
+        event.preventDefault();
+
+        $.ajax({
+            method : "post",
+            url : baseUrl + "/todos",
+            headers : {
+                token : localStorage.token
+            },
+            data : {
+                title : $("#create-todo-title").val(),
+                description : $("#create-todo-description").val(),
+                due_date : $("#create-todo-due-date").val(),
+            }
+        })
+        .done(data => {
+            $("#create-todo-title").val("")
+            $("#create-todo-description").val("")
+            $("#create-todo-due-date").val("")
+            auth()
+            
+        })
+        .fail(err => {
+            err.responseJSON.errors.forEach(element => {
+                $("#create-todo-status").text(element.message)
+            });
+            
+        })
+    })    
 });
+
+/**
+ * ===========================================
+ * Auth, Button : login, register, cancel, create, update, logout
+ * ===========================================
+ */
 
 const auth = () => {
 
@@ -87,10 +101,107 @@ const auth = () => {
         $("#register-page").hide()
         $("#login-page").hide()
         $("#main-page").show()
+        $("#create-todo-form").hide()
+        $("#edit-todo-form").hide()
+        fetchToDos()
     } else {
         $("#landing-page").show()
         $("#register-page").hide()
         $("#login-page").hide()
         $("#main-page").hide()
+        $("#create-todo-form").hide()
+        $("#edit-todo-form").hide()
     }
+}
+
+const login = () => {
+
+    $("#landing-page").hide()
+    $("#register-page").hide()
+    $("#login-page").show()
+    $("#login-status").text("");
+}
+
+const register = () => {
+
+    $("#landing-page").hide()
+    $("#register-page").show()
+    $("#login-page").hide()
+    $("#register-status").text("");
+    $("#register-to-login").hide()
+}
+
+const cancel = () => {
+
+    auth()
+}
+
+const create = () => {
+
+    $("#create-todo-form").show()
+}
+
+const logout = () => {
+
+    localStorage.clear()
+    auth()
+}
+
+/**
+ * ===========================================
+ * Fetch, Create, Delete
+ * ===========================================
+ */
+
+const fetchToDos = () => {
+
+    $.ajax({
+        method : "get",
+        url : baseUrl + "/todos/",
+        headers : {
+            token : localStorage.token
+        }
+    })
+    .done(data => {
+        // console.log(data);
+        $(".card").text("")
+        data.ToDos.forEach(element => {
+            let date = new Date(element.due_date)
+            $(".card").append(`
+            <div class="card-body">
+
+                <h4 class="card-title">${element.title}</h4>
+                <div class="alert alert-warning" role="alert">
+                <strong>Due date : </strong>${date.getDate()}-${date.getMonth()}-${date.getFullYear()}
+                </div>
+                <h6 class="card-subtitle mb-2 text-muted">${element.status ? "COMPLETED !" : "not yet completed"}</h6>
+                <p class="card-text">
+                ${element.description}
+                </p>
+                <button type="button" class="btn btn-primary" onclick="updateToDo(${element.id})">Edit</button>
+                <button type="button" class="btn btn-danger" onclick="deleteToDo(${element.id})">Delete</button><br><br>
+            </div>
+        `)
+        });
+    })
+    .fail(err => {
+        console.log(err.responseJSON.message);
+    })
+}
+
+const deleteToDo = (id) => {
+
+    $.ajax({
+        method : "delete",
+        url : baseUrl + "/todos/" + id,
+        headers : {
+            token : localStorage.token
+        }
+    })
+    .done(data => {
+        fetchToDos()
+    })
+    .fail(err => {
+        console.log(err)
+    })
 }
