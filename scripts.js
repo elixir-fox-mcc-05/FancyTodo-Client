b5a61bf9c25c0f5f8483ea4031422454b2c184dd
Grady Wicoady <nervalgravie@gmail.com>
2020-05-01 18:38:48 +0800

    add pics

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-//    $('#edit').hide()
-//    $('#list').hide()
-//    $('#add').hide()
+  //    $('#edit').hide()
+  //    $('#list').hide()
+  //    $('#add').hide()
   checkStorage()
   $('#login').on('submit', function (event) {
     event.preventDefault()
@@ -9,8 +9,8 @@ $(document).ready(function () {
     const password = $('#loginPassword').val()
     login(email, password)
   })
-    
-    $('#register').on('submit', function (event) {
+
+  $('#register').on('submit', function (event) {
     event.preventDefault()
     let data = {}
     data.first_name = $('#regFirst_Name').val()
@@ -19,14 +19,60 @@ $(document).ready(function () {
     data.password = $('#regPassword').val()
     register(data)
   })
+    $('#login').hide()
+//    $('#home').show()
+    home() 
 })
 
+function home(){
+    $('#homeContent').empty()
+    hideAll()
+    $('#home').show()
+    $.ajax({
+    method: 'GET',
+    url: 'http://localhost:3000/holidays'
+    })
+    
+    .done(response => {
+        console.log(response)
+        let today = new Date()
+        let todaymonth = new Date().toLocaleString('default', { month: 'long' })
+        $('#homeContent').html(`
+            <h1>${todaymonth}<h1>
+            
+        `)
+        for (let i =0; i < (new Date(today.getFullYear(), today.getMonth(),0).getDate()); i+6){
+                $('#homeContent').append(`
+                    <div class="columns">
+                    <div class="column">${i+1}</div>
+                    <div class="column">${i+2}</div>
+                    <div class="column">${i+3}</div>
+                    <div class="column">${i+4}</div>
+                    <div class="column">${i+5}</div>
+                    <div class="column">${i+6}</div>
+                    <div class="column">${i+7}</div>
+                    </div>
+                    `)
+//            if(i % 7 == 0){
+//                    $('#homeContent').append(`
+//                       </div>
+//                        <div class="columns">
+//                    `)
+//            }
+        }
 
+    })
+    
+    .fail(err => {
+      console.log(err.responseJSON)
+    })
+    
+}
 
-function login(email,password){
-//    const email = $('#loginEmail').val()
-//    const password = $('#loginPassword').val()
-$.ajax({
+function login(email, password) {
+  //    const email = $('#loginEmail').val()
+  //    const password = $('#loginPassword').val()
+  $.ajax({
     method: 'POST',
     url: 'http://localhost:3000/login',
     data: {
@@ -35,23 +81,17 @@ $.ajax({
     }
   })
     .done(response => {
-        const token = response.token
-        $('#loginEmail').val('')
-        $('#loginPassword').val('')
-        localStorage.setItem('token', token)
-        checkStorage()
-        // hide landing
-//      $('#landingPage').hide()
-//      $('#signInError').hide()
-      // show dashboard
-//      $('#dashboardPage').show()
+      const token = response.token
+      $('#loginEmail').val('')
+      $('#loginPassword').val('')
+      localStorage.setItem('token', token)
+      checkStorage()
+
 
     })
     .fail(err => {
-    console.log(err.responseJSON)
-//      console.log(err.responseJSON.message, ' <<< error')
-//      $('#loginError').show()
-//      $('#loginError').text(err.responseJSON.message)
+      console.log(err.responseJSON)
+
     })
 }
 
@@ -74,12 +114,14 @@ function checkStorage() {
   }
 }
 
-function hideAll(){
-    $('#edit').hide()
-    $('#list').hide()
-    $('#add').hide()
-    $('#login').hide()
-    $('#register').hide()
+function hideAll() {
+    $('#home').hide()
+  $('#edit').hide()
+  $('#list').hide()
+  $('#add').hide()
+  $('#login').hide()
+  $('#register').hide()
+    $('#delete').hide()
 }
 
 function fetchToDo() {
@@ -92,8 +134,8 @@ function fetchToDo() {
     }
   })
     .done(function (response) {
-    $('#toDoTable').empty()
-    $('#toDoTable').append(`
+      $('#toDoTable').empty()
+      $('#toDoTable').append(`
                 <tr>
                     <td>Title</td>
                     <td>Description</td>
@@ -102,213 +144,249 @@ function fetchToDo() {
                     <td>Actions</td>
                 </tr>`)
       const toDo = response.todos
-      console.log(toDo)
-      $('#listtitle').append(`welcome back ${toDo[0].User.first_name} ${toDo[0].User.last_name}, here's your toDo List`)
-//      console.log(response)
+      // console.log(toDo)
+      $('#listtitle').html(`welcome back ${toDo[0].User.first_name} ${toDo[0].User.last_name}, here's your toDo List`)
+      //      console.log(response)
       toDo.forEach(temp => {
-          var date = new Date(temp.due_date)
-           $('#toDoTable').append(`
+        var date = new Date(temp.due_date)
+        $('#toDoTable').append(`
         <tr>
             <td>${temp.title}</td>
             <td>${temp.description}</td>
             <td>${temp.status ? 'completed' : 'incompleted'}</td>
-            <td>${date.getDate()}-${date.getMonth()}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}</td>
-            <td><button onclick="showEditPage(${temp.id},'${temp.title}','${temp.description}','${temp.due_date}',${temp.status},${temp.UserId})">edit</button>
-            <button onclick="remove(${temp.id})">delete</button></td>
+            <td>${date.getDate()}-${date.getMonth()}-${date.getFullYear()}</td>
+            <td><button class="button is-small is-primary has-background-info" onclick="showEditPage(${temp.id},'${temp.title}','${temp.description}','${temp.due_date}',${temp.status},${temp.UserId})">edit</button>
+            <button  class="button is-small is-primary has-background-danger" onclick="showRemoveConfirm(${temp.id})">delete</button></td>
         </tr><br>
          `)
-      })     
+      })
     })
     .fail(function (err) {
       console.log(err.responseJSON)
     })
 }
 
-function register(userdata){
-    console.log(userdata)
-$.ajax({
+function register(userdata) {
+  console.log(userdata)
+  $.ajax({
     method: 'POST',
     url: 'http://localhost:3000/register',
     data: {
-        first_name : userdata.first_name,
-        last_name : userdata.last_name,
-        email : userdata.email,
-        password : userdata.password
+      first_name: userdata.first_name,
+      last_name: userdata.last_name,
+      email: userdata.email,
+      password: userdata.password
     }
   })
     .done(function (response) {
-        $('#regFirst_Name').val('')
-        $('#regLast_Name').val('')
-        $('#regEmail').val('')
-        $('#regPassword').val('')
-        
-    $('#register').hide()
-    $('#login').show()
+      $('#regFirst_Name').val('')
+      $('#regLast_Name').val('')
+      $('#regEmail').val('')
+      $('#regPassword').val('')
+
+      $('#register').hide()
+      $('#login').show()
 
     })
     .fail(function (err) {
       console.log(err.responseJSON)
-//      console.log(err.responseJSON.message, ' <<< error')
-//      $('#loginError').show()
-//      $('#loginError').text(err.responseJSON.message)
     })
 }
 
 
 
-function logout(){
-    hideAll()
-    $('#toDoHeader').hide()
-    $('#loginHeader').show()
+function logout() {
+  hideAll()
+  $('#toDoHeader').hide()
+  $('#loginHeader').show()
 }
 
-function showLogin(){
-    hideAll()
-    $('#login').show()
+function showLogin() {
+  hideAll()
+  $('#login').show()
 }
 
-function showRegister(){
-    hideAll()
-    $('#register').show()
+function showRegister() {
+  hideAll()
+  $('#register').show()
 }
 
-function showList(){
-    hideAll()
-    $('#list').show()
+function showList() {
+  hideAll()
+  $('#list').show()
 }
 
-function showAddPage(){
-    hideAll()
-    $('#add').show()
+function showAddPage() {
+  hideAll()
+    $('#addErrorHeader').hide()
+  $('#add').show()
 }
 
-function addToDo(){
-    const token = localStorage.getItem('token')
-    
-    let title = $('#addTitle').val()
-    let description = $('#addDescription').val()
-    let due_date = $('#adddue_date').val()
-    
-    console.log(title,description,due_date)
-    $.ajax({
-        method: 'POST',
-        url: 'http://localhost:3000/todos',
-        headers : {
-            token
-        },
-        data: {
-            title,
-            description,
-            due_date
-        }
-      })
+function addToDo() {
+  const token = localStorage.getItem('token')
+
+  let title = $('#addTitle').val()
+  let description = $('#addDescription').val()
+  let due_date = $('#adddue_date').val()
+//  $('#sumitLogin').addClass('is-loading')
+  console.log(title, description, due_date)
+  $.ajax({
+    method: 'POST',
+    url: 'http://localhost:3000/todos',
+    headers: {
+      token
+    },
+    data: {
+      title,
+      description,
+      due_date
+    }
+  })
     .done(response => {
-        fetchToDo()
-        showList()
+      fetchToDo()
+      showList()
     })
     .fail(err => {
-        console.log(err.responseJSON)
+      let error = err.responseJSON.err.split(",")
+      $('#addErrorHeader').show()
+      $('#addError').text(error[0])
+//      console.log(err.responseJSON)
     })
 }
 
-function remove(id){
-     const token = localStorage.getItem('token')
-     $.ajax({
-        method: 'delete',
-        url: `http://localhost:3000/todos/${id}`,
-        params : { id },
-         headers : { token }
-      })
-    .done(response => {
-        fetchToDo()
-         showList()
-    })
-    .fail(err => {
-        console.log(err.responseJSON)
-    })
-    
-}
 
-function showEditPage(id,title,description,due_date,status,UserId){
-    due_date = new Date(due_date)
-    console.log(due_date)
+function showDelete(){
     hideAll()
-    $('#edit').show()
-    $('#edit').append(`
-                    <input type="text" placeholder="title" id="editTitle" value="${title}"><br>
-                    <input type="text" placeholder="description" id="editDescription" value="${description}"><br>
-                    <input type="text" placeholder="due date" id="editdue_date" value="${due_date}"><br>
-                    <input type="radio" id="editStatus" name="status" value="false" ${!status ? "checked" : ""}>not completed
-                    <input type="radio" id="editStatus" name="status" value="true" ${status ? "checked" : ""}>completed
-                    <button class="is-centered" onclick="update('${id}','${title}','${description}','${due_date}','${status}','${UserId}')">Edit</button>
+    $('#delete').show()
+    $('#delete').html(`<div class="box">
+                    <h1 class="hero-body title">Edit ToDo</h1>
+                    <h2 id="editError" class=""></h2>
+                    <input class="input is-medium" type="text" placeholder="title" id="editTitle" value="${title}"><br>
+                    <input class="input is-medium" type="text" placeholder="description" id="editDescription" value="${description}"><br>
+                    <input class="input is-medium" type="text" placeholder="due date" id="editdue_date" value="${due_date}"><br>
+                    <input class="radio" type="radio" id="editStatus" name="status" value="false" ${!status ? "checked" : ""}>not completed
+                    <input class="radio" type="radio" id="editStatus" name="status" value="true" ${status ? "checked" : ""}>completed
+                    <button class="button is-block is-info is-large is-fullwidth" onclick="update('${id}','${title}','${description}','${due_date}','${status}','${UserId}')">Edit</button>
+                    <div>
     `)
-    
-//    $('#').value(`${data.title}`)
-//    $('#').value(`${data.description}`)
-//    $('#').value(`${data.due_date}`)
 }
 
-function update(id){
-     const token = localStorage.getItem('token')
-     
-    title = $('#editTitle').val()
-    description = $('#editDescription').val()
-    due_date = $('#editdue_date').val()
-    status = $("#editStatus[name=status]:checked").val()
-    console.log(status)
-    $.ajax({
-       method: 'put',
-       url: `http://localhost:3000/todos/${id}`,
-        headers : {
-            token
-        },
-        data : {
-            title,
-            description,
-            due_date,
-            status
-        }
-              
-//          success: function(msg){
-//            console.log("eh masuk")
-//            fetchToDo()
-//        }
-     })
-    
-     
+function showRemoveConfirm(id){
+    $('#delete').show()
+    $('#delete').append()
+}
+
+
+function remove(id) {
+  const token = localStorage.getItem('token')
+  $.ajax({
+    method: 'delete',
+    url: `http://localhost:3000/todos/${id}`,
+    params: { id },
+    headers: { token }
+  })
     .done(response => {
-      console.log("eh masuk")
-       fetchToDo()
-        showList()
+      fetchToDo()
+      showList()
     })
-    
     .fail(err => {
-      console.log("eh gak")
-        console.log(err)
+      console.log(err.responseJSON)
     })
-    
-    
-    console.log("bodo")
+
+}
+
+function showEditPage(id, title, description, due_date, status, UserId) {
+  due_date = new Date(due_date)
+  console.log(due_date)
+  hideAll()
+  $('#edit').show()
+  $('#edit').html(`<div class="box">
+                    <h1 class="hero-body title">Edit ToDo</h1>
+                    <h2 id="editError" class=""></h2>
+                    <input class="input is-medium" type="text" placeholder="title" id="editTitle" value="${title}"><br>
+                    <input class="input is-medium" type="text" placeholder="description" id="editDescription" value="${description}"><br>
+                    <input class="input is-medium" type="text" placeholder="due date" id="editdue_date" value="${due_date}"><br>
+                    <input class="radio" type="radio" id="editStatus" name="status" value="false" ${!status ? "checked" : ""}>not completed
+                    <input class="radio" type="radio" id="editStatus" name="status" value="true" ${status ? "checked" : ""}>completed
+                    <button class="button is-block is-info is-large is-fullwidth" onclick="update('${id}','${title}','${description}','${due_date}','${status}','${UserId}')">Edit</button>
+                    <div>
+    `)
+
+}
+
+function update(id) {
+  const token = localStorage.getItem('token')
+
+  title = $('#editTitle').val()
+  description = $('#editDescription').val()
+  due_date = $('#editdue_date').val()
+  status = $("#editStatus[name=status]:checked").val()
+//  console.log(status)
+  $.ajax({
+    method: 'put',
+    url: `http://localhost:3000/todos/${id}`,
+    headers: {
+      token
+    },
+    data: {
+      title,
+      description,
+      due_date,
+      status
+    }
+  })
+
+    .done(response => {
+//      console.log("eh masuk")
+      fetchToDo()
+      showList()
+    })
+
+    .fail(err => {
+      $('#editError').html(err.responseJSON.err)
+//      console.log("eh gak")
+      console.log(err)
+    })
+
+
 }
 
 function logout() {
-//  const auth2 = gapi.auth2.getAuthInstance();
-//  auth2.signOut().then(function () {
-//    localStorage.removeItem('access_token');
-//    $('#main').empty();
-//    $('#login-navbar-button').hide();
-//    $('#not-login-navbar-button').show();
-//    showFormLogin();
-//  });
-    localStorage.clear()
-   
-    checkStorage()
+   const auth2 = gapi.auth2.getAuthInstance();
+   auth2.signOut().then(function () {
+     localStorage.removeItem('access_token');
+     $('#main').empty();
+     $('#login-navbar-button').hide();
+     $('#not-login-navbar-button').show();
+     showFormLogin();
+   });
+  localStorage.clear()
+
+  checkStorage()
 }
 
 function onSignIn(googleUser) {
- var profile = googleUser.getBasicProfile();
- console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
- console.log('Name: ' + profile.getName());
- console.log('Image URL: ' + profile.getImageUrl());
- console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+  const id_token = googleUser.getAuthResponse().id_token;
+
+  $.ajax({
+    method: 'POST',
+    url: 'http://localhost:3000/logingoogle',
+    headers: {
+      google_token: id_token
+    }
+  })
+
+    .done(response => {
+      const token = response.token
+      console.log(response)
+      $('#loginEmail').val('')
+      $('#loginPassword').val('')
+      localStorage.setItem('token', token)
+      checkStorage()
+    })
+
+    .fail(err => {
+      console.log(err.responseJSON)
+    })
 }
