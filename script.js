6b689ccd193e03a7c3bdab4520ccf89f5d6027d8
kalys <61675998+kalyskhairy@users.noreply.github.com>
2020-05-02 16:30:02 +0700

    commit

--- script.js
+++ script.js
@@ -258,3 +258,52 @@ function updateTodo(){
 
 }
 
+// searchMovie
+function searchMovie(){
+    $('#movie-list').html('')
+    $.ajax({
+        url : 'http://omdbapi.com',
+        type : 'get',
+        dataType : 'json',
+        data : {
+            'apikey' : '606c2e47',
+            's' : $('#search-input').val()
+        },
+        success : function(result) {
+            if(result.Response == 'True'){
+                let movies = result.Search;
+                $.each(movies, function(i, data) {
+                    
+                    $('#movie-list').append(`
+                    <div class="col-md-3">
+                        <div class="card">
+                            <img src="`+ data.Poster +`" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">`+ data.Title +`</h5>
+                                <p class="card-text">Type :`+ data.Type +`\nYear :`+ data.Year +`</p>
+                                <a href="${data.imdbID}" class="card-link">See Detail </a>
+                            </div>
+                        </div>
+                    </div>
+                    `)
+                })
+                $('#search-input').val('')
+
+            }else{
+                $('#movie-list').html('<h1 class="text-center">'+ result.Error +'</h1>')
+            }
+        }
+    });
+
+
+}
+
+$('#search-button').on('click', function() {
+    searchMovie()
+});
+
+$('#search-input').on('keyup', function(e) {
+    if(e.keyCode === 13){
+        searchMovie()
+    }
+});
