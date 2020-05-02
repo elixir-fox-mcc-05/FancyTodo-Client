2a5ae29d5ee7bcf5d6be2a9c34c6301438931b5e
amirfaisalz <amirsakti27@gmail.com>
2020-05-02 17:36:33 +0700

    belum berhasil menambahkan google signin

---
+++ scripts/signIn.js
@@ -0,0 +1,7 @@
+// document.querySelector('.img-btn').addEventListener('click', function() {
+//     document.querySelector('.cont').classList.toggle('s-signup');
+//   });
+
+document.querySelector('.img__btn').addEventListener('click', function() {
+  document.querySelector('.cont').classList.toggle('s--signup');
+});
